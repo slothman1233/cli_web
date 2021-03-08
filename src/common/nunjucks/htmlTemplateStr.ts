2b5eb69be130861d0d90ea1e685a8f1a86f193a1
slothman <slothman123@163.com>
2021-03-08 18:16:23 +0800

    1

--- src/common/nunjucks/htmlTemplateStr.ts
+++ src/common/nunjucks/htmlTemplateStr.ts
@@ -4,11 +4,11 @@ import { nunRender } from './index'
 
 export default {
     'FmMoreRead': function (url: string, ...arg: any) {
-       
+
         try {
-            nunRender(url, { models: arg })
             console.log(arg)
-            return 'sdfdsf'
+            return nunRender(url, { models: arg })
+
         } catch (e) {
             return 'sdfdsf'
         }

49aec396b55017652662bdf64e25d94f9506c5cc
wenliang <slothman123@163.com>
2021-04-04 23:43:07 +0800

    新增 nunRenderMacroString

--- src/common/nunjucks/index.ts
+++ src/common/nunjucks/index.ts
@@ -9,6 +9,8 @@ import nunjucks from 'nunjucks'
 import env from '../config/env'
 import filters from './filter'
 import { isAsync } from '../utils/type_check'
+import { isFile, readFile } from '../utils/file'
+import log from '../../middleware/log4js/log'
 /**
  * 把环境变量赋值给 nunjucks 框架当做 nunjucks 的全局变量
  */
@@ -74,6 +76,68 @@ export const nunRenderString = (str: string, optins: any) => {
     return html
 }
 
+
+
+
+
+/**
+ * 通过nunjucks的macro字符串文本跟参数获取编译后的html代码
+ * @param {string} filepath 文件地址 地址相对于macro地址
+ * @param {string} name macro模板名称
+ * @param {any} options 渲染的参数
+ */
+export const nunRenderMacroString = async (filepath: string, name: string, options: any): Promise<string> => {
+    if (!filepath) { return null }
+
+    let reg = /\{\%\s*?macro\s*?([^\(].*?)\(.*?\%\}(.*?)\{\%\s*?endmacro\s*?\%\}/igs
+    let paths = path.resolve(__dirname, '..', '..', 'views/shared/sharedView/macro', filepath)
+    let isfile = await isFile(paths)
+
+    if (!isfile) { return '' }
+
+    let getstring = (await readFile(paths)).toString()
+
+    // name:""
+    let obj = {}
+    let res
+
+
+    let hreadstring = getstring.replace(reg, function ($1, $2, $3): string {
+        obj[$2.trim()] = $3
+        return ''
+    })
+
+    if (!obj[name]) { return '' }
+
+
+    let filep = paths.slice(0, paths.lastIndexOf('\\'))
+
+    hreadstring = hreadstring.replace(/[\'\"](.*?)[\'\"]/igs, function ($1, $2): string {
+        let p = path.resolve(filep, $2)
+        let pathArg = p.split('\\').filter(function (s) {
+            return s && s.trim()
+        })
+
+        let paths=''
+
+        if(path.sep === '/'){
+            paths = pathArg.join('/')
+        }else{
+            paths = pathArg.join('\\\\')
+        }
+
+        return `"${paths}"`
+    })
+
+    let html = ''
+    try {
+        html = nunRenderString(hreadstring + obj[name], options)
+    } catch (e) {
+        log.error({ error: e.stack.toString(), resTime: Date.now() })
+    }
+
+    return html
+}
 
 
 
